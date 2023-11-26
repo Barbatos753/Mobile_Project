b1e258af4fe658ced998a798f0e5ffa307807ae6
Barbatos753 <151723822+Barbatos753@users.noreply.github.com>
2023-11-26 20:01:31 +0200

    Update FoodGamemode.dart

--- FoodGamemode.dart
+++ FoodGamemode.dart
@@ -1,191 +1,247 @@
 import 'package:flutter/material.dart';
-import 'package:johnnyroudy/FoodGamemode.dart';
+import 'dart:math';
+import 'main.dart';
 import 'Food.dart';
 
-Food carrot = Food(
-  image: 'images/carrot.jpg',
-  fact: 'Carrots support eye health with beta-carotene.',
-);
-
-Food broccoli = Food(
-  image: 'images/broccoli.jpg',
-  fact: 'Broccoli is high in vitamins K and C for bone health.',
-);
-
-Food cucumber = Food(
-  image: 'images/cucumber.jpg',
-  fact: 'Cucumbers are low-calorie and hydrating for skin health.',
-);
-
-Food tomato = Food(
-  image: 'images/tomato.jpg',
-  fact: 'Tomatoes contain lycopene, an antioxidant for heart health.',
-);
-
-Food bellPepper = Food(
-  image: 'images/bellpeper.jpg',
-  fact: 'Bell peppers provide vitamin A and C for immune support.',
-);
-
-Food spinach = Food(
-  image: 'images/spinach.jpg',
-  fact: 'Spinach is rich in iron and vitamins for energy.',
-);
-
-Food lettuce = Food(
-  image: 'images/lettuce.jpg',
-  fact: 'Lettuce is a low-calorie green that adds crunch to salads.',
-);
-
-Food olives = Food(
-  image: 'images/olives.jpg',
-  fact: 'Olives offer healthy monounsaturated fats for heart health.',
-);
-
-Food apple = Food(
-  image: 'images/apple.jpg',
-  fact: 'Apples, high in fiber, promote digestive health.',
-);
-
-Food banana = Food(
-  image: 'images/banana.jpg',
-  fact: 'Bananas are potassium-rich for heart and muscle health.',
-);
-
-Food orange = Food(
-  image: 'images/orange.jpg',
-  fact: 'Oranges are packed with vitamin C for immune support.',
-);
-
-Food grape = Food(
-  image: 'images/grape.jpg',
-  fact: 'Grapes contain antioxidants for overall health.',
-);
-
-Food strawberry = Food(
-  image: 'images/strawberry.jpg',
-  fact: 'Strawberries are rich in vitamin C and antioxidants.',
-);
-
-Food watermelon = Food(
-  image: 'images/watermelon.jpg',
-  fact: 'Watermelons are hydrating fruits with a sweet taste.',
-);
-
-Food pineapple = Food(
-  image: 'images/pineapple.jpg',
-  fact: 'Pineapples are tropical fruits with digestive benefits.',
-);
-
-Food coconut = Food(
-  image: 'images/coconut.jpg',
-  fact: 'Coconuts provide healthy fats and are versatile in cooking.',
-);
-List<Food> Vegetables=[cucumber,carrot,lettuce,tomato,spinach,olives,bellPepper,broccoli];
-List<Food> Fruits=[apple,banana,coconut,pineapple,strawberry,grape,orange,watermelon];
-void main() {
-  runApp(MyApp());
+class FoodGamemode extends StatefulWidget {
+  final String difficultyValue;
+  FoodGamemode({required this.difficultyValue});
+  @override
+  FoodGamemodeState createState() => FoodGamemodeState(difficultyValue: difficultyValue);
 }
 
-class MyApp extends StatelessWidget {
+class FoodGamemodeState extends State<FoodGamemode> {
+  List<Food> options = [];
+  Food correctAnswer = Food(image: '', fact: '');
+  int count = 1;
+  final String difficultyValue;
+  String fruitOrVegetable = '';
+  FoodGamemodeState({required this.difficultyValue});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyPage(),
-    );
+  void initState() {
+    super.initState();
+    startNewLevel();
   }
-}
 
-class MyPage extends StatefulWidget {
-  const MyPage({super.key});
+  void startNewLevel() {
+    options = [];
+    fruitOrVegetable = ['Fruit', 'Vegetable'][Random().nextInt(2)];
+    Fruits.shuffle();
+    Vegetables.shuffle();
 
-  @override
-  MyPageState createState() => MyPageState();
-}
-
-class MyPageState extends State<MyPage> {
-  String difficultyValue = "Select Difficulty";
-  String chooseOptionValue = 'Choose Option';
+    if (fruitOrVegetable == 'Fruit') {
+      if (difficultyValue == 'Easy') {
+        options.add(Fruits[Random().nextInt(Fruits.length)]);
+        for (Food item in Vegetables) {
+          if (!options.contains(item)) {
+            options.add(item);
+          }
+          if (options.length == 4) {
+            break;
+          }
+        }
+      } else if (difficultyValue == 'Medium') {
+        options.add(Fruits[Random().nextInt(Fruits.length)]);
+        for (Food item in Vegetables) {
+          if (!options.contains(item)) {
+            options.add(item);
+          }
+          if (options.length == 6) {
+            break;
+          }
+        }
+      } else if (difficultyValue == 'Hard') {
+        options.add(Fruits[Random().nextInt(Fruits.length)]);
+        for (Food item in Vegetables) {
+          if (!options.contains(item)) {
+            options.add(item);
+          }
+          if (options.length == 8) {
+            break;
+          }
+        }
+      }
+    } else {
+      if (difficultyValue == 'Easy') {
+        options.add(Vegetables[Random().nextInt(Vegetables.length)]);
+        for (Food item in Fruits) {
+          if (!options.contains(item)) {
+            options.add(item);
+          }
+          if (options.length == 4) {
+            break;
+          }
+        }
+      }
+      if (difficultyValue == 'Medium') {
+        options.add(Vegetables[Random().nextInt(Vegetables.length)]);
+        for (Food item in Fruits) {
+          if (!options.contains(item)) {
+            options.add(item);
+          }
+          if (options.length == 6) {
+            break;
+          }
+        }
+      }
+      if (difficultyValue == 'Hard') {
+        options.add(Vegetables[Random().nextInt(Vegetables.length)]);
+        for (Food item in Fruits) {
+          if (!options.contains(item)) {
+            options.add(item);
+          }
+          if (options.length == 8) {
+            break;
+          }
+        }
+      }
+    }
+    correctAnswer = options[0];
+    options.shuffle();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Background Image
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('images/homebackground.jpeg'), // Replace with your background image path
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          // Content
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                // Small Image
-                Image.asset(
-                  'images/gm1.png', // Replace with your small image path
-                  width: 200, // Adjust width as needed
-                  height: 200, // Adjust height as needed
-                ),
-
-                SizedBox(height: 20),
-                // Difficulty Dropdown
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      DropdownButton<String>(
-                        items:['Select Difficulty', 'Easy', 'Medium', 'Hard']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        })
-                            .toList(),
-                        onChanged: (String? value) {
-                          setState(() {
-                            difficultyValue = value !;
-                          });
-                        },
-                        value: difficultyValue,
-                      ),
-                      SizedBox(width: 60),
-                      // Choose Option Dropdown
-                      DropdownButton<String>(
-                        items: ['Choose Option', 'Choose Right', 'Think Tight']
-                            .map((option) => DropdownMenuItem<String>(
-                          value: option,
-                          child: Text(option),
-                        ))
-                            .toList(),
-                        onChanged: (String? value) {
-                          setState(() {
-                            chooseOptionValue = value ?? '';
-                          }
-                          );
-                        },
-                        value: chooseOptionValue,
-                      ),
-                    ]),
-                ElevatedButton(
-                  onPressed: () {
-                    // Handle button press
-                    Navigator.push(context,MaterialPageRoute(builder: (context) =>  FoodGamemode(difficultyValue: difficultyValue)));
-                  },
-                  child: Text('Start'),
-                ),
-              ],
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        title: Text('Choose Right'),
       ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/back2.webp'), // Replace with your background image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Choose The $fruitOrVegetable: Level $count',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              // Use a Row to display two columns with a SingleChildScrollView
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      children: options.sublist(0, options.length ~/ 2).map((option) {
+                        return buildImageContainer(option);
+                      }).toList(),
+                    ),
+                  ),
+                  SizedBox(width: 16), // Add some spacing between columns
+                  SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      children: options.sublist(options.length ~/ 2).map((option) {
+                        return buildImageContainer(option);
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildImageContainer(Food option) {
+    return InkWell(
+      onTap: () {
+        if (option == correctAnswer) {
+          count++;
+          if (count <= 5) {
+            showResultMessage(true);
+          } else
+            showmax(context);
+        } else {
+          showResultMessage(false);
+          count = 1;
+        }
+      },
+      child: Container(
+        margin: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.yellow, width: 2),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.asset(
+            option.image,
+            width: 80,
+            height: 80,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void showResultMessage(bool isCorrect) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Result'),
+          content: Text(isCorrect ? 'Correct Answer!' : 'Wrong Answer!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                startNewLevel();
+                setState(() {
+
+                });
+                Navigator.pop(context);
+              },
+              child: Text('Next'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showmax(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('You Won!'),
+          content: Text('You Completed This Level, Congrats!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => MyPage()));
+              },
+              child: Text('Go to main'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
 
+class AnotherPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Another Page'),
+      ),
+      body: Center(
+        child: Text('This is another page.'),
+      ),
+    );
+  }
+}
