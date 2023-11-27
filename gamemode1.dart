5528346e5cfad7f946d41970aaba4086a682cffd
Barbatos753 <151723822+Barbatos753@users.noreply.github.com>
2023-11-27 06:06:36 +0200

    Update gamemode1.dart

--- gamemode1.dart
+++ gamemode1.dart
@@ -231,17 +231,3 @@ class FoodGamemodeState extends State<FoodGamemode> {
     );
   }
 }
-
-class AnotherPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Another Page'),
-      ),
-      body: Center(
-        child: Text('This is another page.'),
-      ),
-    );
-  }
-}
